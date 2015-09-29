d6f0961d6c6493a53ae41ef89f9fed091d97eeaf
Youye <sun.yo@husky.neu.edu>
2015-09-29 01:42:29 -0400

    add datamap and connect d3 with redis as data resource

--- web/app.py
+++ web/app.py
@@ -11,6 +11,20 @@ def show_homepage():
     return render_template("map.html")
 
 
+def event_stream():
+    pubsub = r.pubsub()
+    pubsub.subscribe('SentimentTweetTopology')
+    for message in pubsub.listen():
+        print message
+        yield 'data: %s\n\n' % message['data']
+
+
+#for d3 data source
+@app.route('/stream')
+def stream():
+    return Response(event_stream(), mimetype="text/event-stream")
+
+
 if __name__ == '__main__':
     app.run(threaded=True,
     host='0.0.0.0'
